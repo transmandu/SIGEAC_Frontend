b2081e2dabac390c0733d3c723d89a4b3217c704
aeaf-transmandu <aaguinagalde@transmandu.com>
2025-06-05 08:59:43 -0400

    adaptando actividad sms

---
+++ hooks/sms/useGetSMSActivityById.ts
@@ -0,0 +1,19 @@
+import axiosInstance from "@/lib/axios";
+import { SMSActivity } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchSMSActivityById = async (id: string | number) => {
+  const { data } = await axiosInstance.get(
+    `transmandu/sms/sms-activity/${id}`
+  );
+  return data;
+};
+
+
+export const useGetSMSActivityById = (id: string | number) => {
+  return useQuery<SMSActivity>({
+    queryKey: ["sms-activity", id], // Incluye el ID en la clave de la query
+    queryFn: () => fetchSMSActivityById(id), // Pasa el ID a la función fetchUser
+    staleTime: 1000 * 60 * 5, // 5 minutos
+  });
+};
