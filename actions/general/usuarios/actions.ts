20750423d946727dc096ac083ab9ee94080659cc
hguzman <hguzman@transmandu.com>
2025-06-20 15:06:14 -0400

    feat: refactor user creation and update actions, streamline form schema and mutation handling

--- actions/general/usuarios/actions.ts
+++ actions/general/usuarios/actions.ts
@@ -1,40 +1,69 @@
-import axiosInstance from "@/lib/axios";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { toast } from "sonner";
+import axiosInstance from "@/lib/axios"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { toast } from "sonner"
 
-interface CreateEmployeeFormSchema {
-  first_name: string;
-  last_name: string;
-  dni: string;
-  company: string;
-  job_title: {
-    id: number;
-  };
-  department: {
-    id: number;
-  };
-  location: {
-    id: number;
-  };
+interface CreateFormSchema {
+    first_name: string,
+    last_name: string,
+    username: string;
+    email: string;
+    isActive: boolean;
+  }
+
+export const useCreateUser = () => {
+
+    const queryClient = useQueryClient()
+
+    const createMutation = useMutation({
+        mutationFn: async (data: CreateFormSchema) => {
+            await axiosInstance.post('/register', data)
+          },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['users']})
+            toast.success("¡Creado!", {
+                description: `¡El usuario ha sido creado correctamente!`
+            })
+          },
+        onError: (e) => {
+            toast.error("Oops!", {
+              description: "¡Hubo un error al crear el usuario!"
+          })
+          },
+        }
+    )
+
+    return {
+      createUser: createMutation,
+    }
 }
 
-export const useCreateEmployee = () => {
-  const queryClient = useQueryClient();
+export const useUpdateUser = () => {
+  const queryClient = useQueryClient()
+  const createMutation = useMutation({
+      mutationFn: async (data: {
+        username: string,
+        email: string,
+        password: string,
+        id: string,
+      }) => {
+          await axiosInstance.put(`/user/${data.id}`, data)
+        },
+      onSuccess: () => {
+          queryClient.invalidateQueries({queryKey: ['users']})
+          queryClient.invalidateQueries({queryKey: ['user']})
+          toast.success("¡Actualizado!", {
+              description: `¡El usuario ha sido actualizado correctamente!`
+          })
+        },
+      onError: (e) => {
+          toast.error("Oops!", {
+            description: "¡Hubo un error al actualizar el usuario!"
+        })
+        },
+      }
+  )
 
-  return useMutation({
-    mutationFn: async (data: CreateEmployeeFormSchema) => {
-      await axiosInstance.post("/employees", data);
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["employees"] });
-      toast.success("¡Creado!", {
-        description: `¡El empleado ha sido creado correctamente!`,
-      });
-    },
-    onError: () => {
-      toast.error("Oops!", {
-        description: "¡Hubo un error al crear el empleado!",
-      });
-    },
-  });
-};
+  return {
+    updateUser: createMutation,
+  }
+}
