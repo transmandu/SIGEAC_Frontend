24a9f47f38918ad4be7b8fe17286acf41c83c4bd
Angel Anton <it@transmandu.com>
2025-07-02 11:19:32 -0400

    NUMBER Y TOFIXED

--- components/dialogs/FlightResumeDialog.tsx
+++ components/dialogs/FlightResumeDialog.tsx
@@ -93,11 +93,11 @@ const FlightResumeDialog = ({ flight }: { flight: Flight }) => {
               <div className="space-y-3">
                 <div className="flex justify-between">
                   <span className="text-sm">Tarifa:</span>
-                  <span className="font-medium">{flight.fee.toFixed(2)}</span>
+                  <span className="font-medium">{Number(flight.fee).toFixed(2)}</span>
                 </div>
                 <div className="flex justify-between">
                   <span className="text-sm">Costo Total:</span>
-                  <span className="font-bold">{flight.total_amount.toFixed(2)}</span>
+                  <span className="font-bold">{Number(flight.total_amount).toFixed(2)}</span>
                 </div>
                 <div className="flex justify-between">
                   <span className="text-sm">Pagado:</span>
