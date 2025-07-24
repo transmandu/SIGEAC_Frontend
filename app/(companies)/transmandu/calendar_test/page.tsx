6374ea7754c2f9e29cc84283afd5a7c6461ec033
aeaf-transmandu <aaguinagalde@transmandu.com>
2025-07-24 09:30:45 -0400

    calendar test

---
+++ app/(companies)/transmandu/calendar_test/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import BigCalendar from "@/components/misc/BigCalendarComponent";
+
+const TestPage = () => (
+  <div className="flex items-center justify-center">
+    <BigCalendar />
+  </div>
+);
+export default TestPage;
