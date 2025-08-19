3a3bc43622571cb2b612dffe1841bfb86d63f5e8
Angel Anton <it@transmandu.com>
2025-08-19 11:07:45 -0400

    push de merge

--- components/forms/mantenimiento/compras/CreateGeneralRequisitionForm.tsx
+++ components/forms/mantenimiento/compras/CreateGeneralRequisitionForm.tsx
@@ -7,6 +7,7 @@ import { useAuth } from "@/contexts/AuthContext"
 import { useGetSecondaryUnits } from "@/hooks/general/unidades/useGetSecondaryUnits"
 import { useGetBatchesByLocationId } from "@/hooks/mantenimiento/almacen/renglones/useGetBatchesByLocationId"
 import { useGetMaintenanceAircrafts } from '@/hooks/mantenimiento/planificacion/useGetMaintenanceAircrafts'
+import { useGetUserDepartamentEmployees } from "@/hooks/sistema/empleados/useGetUserDepartamentEmployees"
 import { cn } from "@/lib/utils"
 import { useCompanyStore } from "@/stores/CompanyStore"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -701,4 +702,3 @@ export function CreateGeneralRequisitionForm({
     </Form>
   );
 }
-
