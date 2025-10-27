83f6d77ea0971344a8580df19e9a5d8ab35bacf7
aeaf-transmandu <aaguinagalde@transmandu.com>
2025-10-27 10:49:59 -0400

    feat:  menu to edit and delete articles

--- components/dropdowns/mantenimiento/almacen/ArticleDropdownActions.tsx
+++ components/dropdowns/mantenimiento/almacen/ArticleDropdownActions.tsx
@@ -2,30 +2,39 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu"
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
+import { useDeleteArticle } from "@/actions/mantenimiento/almacen/inventario/articulos/actions";
+import { useCompanyStore } from "@/stores/CompanyStore";
+import { Loader2, MoreHorizontal, SquarePen, Trash2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
-import { useDeleteArticle } from "@/actions/mantenimiento/almacen/inventario/articulos/actions"
-import { EditIcon, EyeIcon, Loader2, MoreHorizontal, Trash2 } from "lucide-react"
-import { useRouter } from "next/navigation"
-import { useState } from "react"
-import { Button } from "../../../ui/button"
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../../../ui/dialog"
-import { useCompanyStore } from "@/stores/CompanyStore"
-
-const ArticleDropdownActions = ({ id, serial, part_number }: { id: string | number, serial: string, part_number: string }) => {
-
-  const [open, setOpen] = useState<boolean>(false)
-  const router = useRouter()
-  const { selectedCompany } = useCompanyStore()
-  const { deleteArticle } = useDeleteArticle()
+const ArticleDropdownActions = ({ id }: { id: string | number }) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const router = useRouter();
+  const { selectedCompany } = useCompanyStore();
+  const { deleteArticle } = useDeleteArticle();
 
   const handleDelete = (id: number | string) => {
-    deleteArticle.mutate({id, company: selectedCompany!.slug}, {
-      onSuccess: () => setOpen(false), // Cierra el modal solo si la eliminación fue exitosa
-    });
-  }
+    deleteArticle.mutate(
+      { id, company: selectedCompany!.slug },
+      {
+        onSuccess: () => setOpen(false), // Cierra el modal solo si la eliminación fue exitosa
+      }
+    );
+  };
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -36,40 +45,60 @@ const ArticleDropdownActions = ({ id, serial, part_number }: { id: string | numb
             <MoreHorizontal className="h-4 w-4" />
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent align="center" className="flex gap-2 justify-center">
-          <DropdownMenuItem className="cursor-pointer" onClick={() => {
-            router.push(`/${selectedCompany?.slug}/almacen/inventario/gestion/${part_number}/${serial}/editar`)
-          }}>
-            <EditIcon className="size-5 text-blue-500" />
-          </DropdownMenuItem>
-          <DropdownMenuItem className="cursor-pointer" onClick={() => {
-            router.push(`/${selectedCompany?.slug}/almacen/inventario/gestion/${part_number}/${serial}`)
-          }}>
-            <EyeIcon className="size-5" />
+        <DropdownMenuContent
+          align="center"
+          className="flex gap-2 justify-center"
+        >
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={() => {
+              router.push(
+                `/${selectedCompany?.slug}/almacen/inventario_articulos/editar/${id}`
+              );
+            }}
+          >
+            <SquarePen className="size-5" />
           </DropdownMenuItem>
           <DialogTrigger asChild>
             <DropdownMenuItem className="cursor-pointer">
-              <Trash2 className='size-5 text-red-500' />
+              <Trash2 className="size-5 text-red-500" />
             </DropdownMenuItem>
           </DialogTrigger>
         </DropdownMenuContent>
       </DropdownMenu>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle className="text-center">¿Seguro que desea eliminar el articulo?</DialogTitle>
+          <DialogTitle className="text-center">
+            ¿Seguro que desea eliminar el articulo?
+          </DialogTitle>
           <DialogDescription className="text-center p-2 mb-0 pb-0">
-            Esta acción es irreversible y estaría eliminando por completo el articulo seleccionado.
+            Esta acción es irreversible y estaría eliminando por completo el
+            articulo seleccionado.
           </DialogDescription>
         </DialogHeader>
         <DialogFooter className="flex flex-col gap-2 md:gap-0">
-          <Button className="bg-rose-400 hover:bg-white hover:text-black hover:border hover:border-black" onClick={() => setOpen(false)} type="submit">Cancelar</Button>
-          <Button disabled={deleteArticle.isPending} className="hover:bg-white hover:text-black hover:border hover:border-black transition-all" onClick={() => handleDelete(id)}>{deleteArticle.isPending ? <Loader2 className="size-4 animate-spin" /> : <p>Confirmar</p>}</Button>
+          <Button
+            className="bg-rose-400 hover:bg-white hover:text-black hover:border hover:border-black"
+            onClick={() => setOpen(false)}
+            type="submit"
+          >
+            Cancelar
+          </Button>
+          <Button
+            disabled={deleteArticle.isPending}
+            className="hover:bg-white hover:text-black hover:border hover:border-black transition-all"
+            onClick={() => handleDelete(id)}
+          >
+            {deleteArticle.isPending ? (
+              <Loader2 className="size-4 animate-spin" />
+            ) : (
+              <p>Confirmar</p>
+            )}
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
+  );
+};
 
-
-  )
-}
-
-export default ArticleDropdownActions
+export default ArticleDropdownActions;
