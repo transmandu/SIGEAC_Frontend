5a2d42a5f1532d60dbdd58ddf275ab2abc890cdc
aeaf-transmandu <aaguinagalde@transmandu.com>
2025-10-27 10:51:45 -0400

    feat: forms to edit articles

---
+++ app/[company]/almacen/inventario_articulos/editar/[id]/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import RegisterArticleForm from '@/components/forms/mantenimiento/almacen/RegisterArticleForm';
+import { ContentLayout } from '@/components/layout/ContentLayout';
+import LoadingPage from '@/components/misc/LoadingPage';
+import { useGetArticleById } from '@/hooks/mantenimiento/almacen/articulos/useGetArticleById';
+import { useCompanyStore } from '@/stores/CompanyStore';
+import { redirect, useParams } from 'next/navigation';
+
+const ConfirmIncomingPage = () => {
+  const params = useParams<{ id: string }>();
+  const { selectedCompany } = useCompanyStore();
+  const { data, isLoading, isError } = useGetArticleById(params.id, selectedCompany?.slug);
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+  if (isError) {
+    redirect(`/${selectedCompany?.slug}/dashboard`);
+  }
+  return (
+    <ContentLayout title="Confirmar Ingreso">
+      {data?.batches?.category}
+      <RegisterArticleForm isEditing initialData={data} category={data?.batches?.category} />
+    </ContentLayout>
+  );
+};
+
+export default ConfirmIncomingPage;
--- components/forms/mantenimiento/almacen/RegisterArticleForm.tsx
+++ components/forms/mantenimiento/almacen/RegisterArticleForm.tsx
@@ -1,66 +1,82 @@
-'use client'
+"use client";
 
-import { Article, Batch, Convertion } from '@/types';
-import { useState } from 'react';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../../../ui/select';
-import CreateComponentForm from './CreateComponentForm';
-import CreateConsumableForm from './CreateConsumableForm';
-import CreateToolForm from './CreateToolForm';
+import { Article, Batch, Convertion } from "@/types";
+import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../../../ui/select";
+import CreateComponentForm from "./CreateComponentForm";
+import CreateConsumableForm from "./CreateConsumableForm";
+import CreateToolForm from "./CreateToolForm";
 
-interface EditingArticle extends Article {
-  batches: Batch,
+export interface EditingArticle extends Article {
+  batches: Batch;
   tool?: {
-    id: number,
-    serial: string,
-    isSpecial: boolean,
-    article_id: number,
-  }
+    id: number;
+    serial: string;
+    isSpecial: boolean;
+    needs_calibration: boolean;
+    last_calibration_date?: string;
+    next_calibration?: string | number;
+    article_id: number;
+  };
   component?: {
-    serial: string,
+    serial: string;
     hard_time: {
-      hour_date: string,
-      cycle_date: string,
-      calendary_date: string,
-    },
+      hour_date: string;
+      cycle_date: string;
+      calendar_date: string;
+    };
     shell_time: {
-      caducate_date: string,
-      fabrication_date: string,
-    }
-  },
+      caducate_date: string;
+      fabrication_date: string;
+      calendar_date: string;
+    };
+  };
   consumable?: {
-    article_id: number,
-    is_managed: boolean,
-    convertions: Convertion[],
-    quantity: number,
-    shell_time: {
-      caducate_date: Date,
-      fabrication_date: string,
-      consumable_id: string,
-    }
-  },
+    lot_number?: string;
+    caducate_date: string;
+    fabrication_date: string;
+  };
 }
 
 interface IRegisterArticleProps {
-  isEditing?: boolean,
-  initialData?: EditingArticle,
-  category?: string,
+  isEditing?: boolean;
+  initialData?: EditingArticle;
+  category?: string;
 }
 
-const RegisterArticleForm = ({ isEditing = false, initialData }: IRegisterArticleProps) => {
-
-  const [type, setType] = useState(initialData?.batches?.category?.toLowerCase() ?? "consumible");
+const RegisterArticleForm = ({
+  isEditing = false,
+  initialData,
+}: IRegisterArticleProps) => {
+  const [type, setType] = useState(
+    initialData?.batches.category.toLowerCase() ?? "componente"
+  );
   function handleTypeSelect(data: string) {
     setType(data);
   }
   return (
-    <div className='space-y-3 mb-4'>
-      <h1 className='font-bold text-3xl'>{isEditing ? "Confirmacion de Articulo" : "Ingreso de Articulo"}</h1>
-      {
-        !isEditing && <p className='text-sm text-muted-foreground'>Seleccione el tipo de articulo a registrar:</p>
-      }
-      <Select disabled={isEditing} defaultValue={initialData?.batches?.category?.toLowerCase()} onValueChange={handleTypeSelect}>
+    <div className="space-y-3 mb-4">
+      <h1 className="font-bold text-3xl">
+        {isEditing ? "Confirmacion de Articulo" : "Ingreso de Articulo"}
+      </h1>
+      {!isEditing && (
+        <p className="text-sm text-muted-foreground">
+          Seleccione el tipo de articulo a registrar:
+        </p>
+      )}
+      <Select
+        disabled={isEditing}
+        value={type}
+        onValueChange={handleTypeSelect}
+      >
         <SelectTrigger className="w-[230px]">
-          <SelectValue placeholder='Seleccionar...' />
+          <SelectValue placeholder="Seleccionar..." />
         </SelectTrigger>
         <SelectContent>
           <SelectItem value="consumible">Consumible</SelectItem>
@@ -68,16 +84,16 @@ const RegisterArticleForm = ({ isEditing = false, initialData }: IRegisterArticl
           <SelectItem value="componente">Componente</SelectItem>
         </SelectContent>
       </Select>
-      {
-        type === 'consumible' && <CreateConsumableForm isEditing={isEditing} initialData={initialData} />
-      }
-      {
-        type === 'herramienta' && <CreateToolForm isEditing={isEditing} initialData={initialData} />
-      }
-      {
-        type === 'componente' && <CreateComponentForm isEditing={isEditing} initialData={initialData} />
-      }
+      {type === "consumible" && (
+        <CreateConsumableForm isEditing={isEditing} initialData={initialData} />
+      )}
+      {type === "herramienta" && (
+        <CreateToolForm isEditing={isEditing} initialData={initialData} />
+      )}
+      {type === "componente" && (
+        <CreateComponentForm isEditing={isEditing} initialData={initialData} />
+      )}
     </div>
-  )
-}
-export default RegisterArticleForm
+  );
+};
+export default RegisterArticleForm;
