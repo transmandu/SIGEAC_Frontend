744b8c6584503f6cdb8044f02433f2f95fd98440
hguzman <hguzman@transmandu.com>
2025-10-27 16:20:30 -0400

    fix: add JEFE_ALMACEN and ANALISTA_ALMACEN roles to menu item for improved access control

--- lib/menu-list-2.tsx
+++ lib/menu-list-2.tsx
@@ -896,7 +896,7 @@ export function getMenuList(
               href: "/ajustes/globales/unidades",
               label: "Unidades",
               active: pathname === "/ajustes/globales/unidades",
-              roles: [ "SUPERUSER", "ENGINEERING"],
+              roles: ["JEFE_ALMACEN", "ANALISTA_ALMACEN", "SUPERUSER", "ENGINEERING"],
             },
             {
               href: "/ajustes/globales/fabricantes",
