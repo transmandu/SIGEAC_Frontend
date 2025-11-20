6be58fb4a1f5904071a38f802ed1b9cd36aafddb
Benjamin Ortiz <bortiz@transmandu.com>
2025-11-20 16:49:37 -0400

    feat: refactor CreateDangerIdentificationDialog to use Dialog component for improved UI interaction

--- components/dialogs/aerolinea/sms/CreateDangerIdentificationDialog.tsx
+++ components/dialogs/aerolinea/sms/CreateDangerIdentificationDialog.tsx
@@ -1,5 +1,16 @@
 "use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import { DangerIdentification } from "@/types";
 import CreateDangerIdentificationForm from "@/components/forms/aerolinea/sms/CreateIdentificationForm";
 
@@ -11,22 +22,44 @@ interface FormProps {
   reportType: string;
 }
 
-export default function CreateDangerIdentificationPage({
+export default function CreateDangerIdentificationDialog({
   title,
   id,
   isEditing,
   initialData,
   reportType,
 }: FormProps) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Card className="flex flex-col p-4 max-w-6xl mx-auto">
-      <h2 className="text-lg font-semibold mb-4">{title}</h2>
-      <CreateDangerIdentificationForm
-        id={id}
-        initialData={initialData}
-        isEditing={isEditing}
-        reportType={reportType}
-      />
+    <Card className="flex">
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button
+            onClick={() => setOpen(true)}
+            variant="outline"
+            size="sm"
+            className="hidden h-8 lg:flex"
+          >
+            {title}
+          </Button>
+        </DialogTrigger>
+
+        <DialogContent className="sm:max-w-[65%] max-h-[80vh] overflow-y-auto">
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+          </DialogHeader>
+
+          <div className="mt-2">
+            <CreateDangerIdentificationForm
+              id={id}
+              initialData={initialData}
+              isEditing={isEditing}
+              reportType={reportType}
+            />
+          </div>
+        </DialogContent>
+      </Dialog>
     </Card>
   );
 }
