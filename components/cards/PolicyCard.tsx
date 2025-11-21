0038c825e40c1b2c235d10ef6adf5c10bd477df3
aeaf-transmandu <aaguinagalde@transmandu.com>
2025-11-21 16:54:57 -0400

    feat: add PolicyCard component for displaying policy information with optional icon, title, description, and action link

--- components/cards/PolicyCard.tsx
+++ components/cards/PolicyCard.tsx
@@ -48,45 +48,51 @@ export function PolicyCard({
     <Card className={cn("w-full flex flex-col", className)}>
       <CardContent className="p-6 flex flex-col flex-grow">
         {/* Icono */}
-        {Icon && (
-          <div className="mb-4 flex justify-center">
-            <div
+
+        <div className="flex sm:flex-col items-center gap-2 ">
+          <div>
+            {Icon && (
+              <div className="mb-4 flex justify-center">
+                <div
+                  className={cn(
+                    "p-3 rounded-full bg-primary/10 flex items-center justify-center",
+                    iconClassName
+                  )}
+                >
+                  <Icon
+                    size={iconSize}
+                    color={iconColor}
+                    className="text-primary"
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div>
+            {" "}
+            {/* Título (opcional) */}
+            {title && (
+              <h3
+                className={cn(
+                  "text-lg font-semibold mb-2 text-center",
+                  titleClassName
+                )}
+              >
+                {title}
+              </h3>
+            )}
+            {/* Descripción */}
+            <p
               className={cn(
-                "p-3 rounded-full bg-primary/10 flex items-center justify-center",
-                iconClassName
+                "text-sm sm:text-base text-left sm:text-justify flex-grow",
+                descriptionClassName
               )}
             >
-              <Icon
-                size={iconSize}
-                color={iconColor}
-                className="text-primary"
-              />
-            </div>
+              {description}
+            </p>
           </div>
-        )}
-
-        {/* Título (opcional) */}
-        {title && (
-          <h3
-            className={cn(
-              "text-lg font-semibold mb-2 text-center",
-              titleClassName
-            )}
-          >
-            {title}
-          </h3>
-        )}
-
-        {/* Descripción */}
-        <p
-          className={cn(
-            "text-sm sm:text-base text-center flex-grow",
-            descriptionClassName
-          )}
-        >
-          {description}
-        </p>
-
+        </div>
         {/* Botón de acción (opcional) */}
         {actionLink && (
           <div className="mt-4 flex justify-center">
