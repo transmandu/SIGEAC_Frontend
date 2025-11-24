69400ec3c60d7eb0e49887911be79b6aa87ddf57
Benjamin Ortiz <bortiz@transmandu.com>
2025-11-24 14:36:55 -0400

    refactor: remove unused import and comment out permissions menu item

--- lib/menu-list-2.tsx
+++ lib/menu-list-2.tsx
@@ -1066,11 +1066,11 @@ export function getMenuList(
               label: "Administrar Roles",
               active: pathname === "/sistema/usuarios_permisos/roles",
             },
-            {
-              href: "/sistema/usuarios_permisos/permisos",
-              label: "Administrar Permisos",
-              active: pathname === "/sistema/usuarios_permisos/permisos",
-            },
+            // {
+            //   href: "/sistema/usuarios_permisos/permisos",
+            //   label: "Administrar Permisos",
+            //   active: pathname === "/sistema/usuarios_permisos/permisos",
+            // },
           ],
         },
         {
