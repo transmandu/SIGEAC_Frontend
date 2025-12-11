a02cf9b8ba4e2153e731c1e96e3958221a1aa215
aeaf-transmandu <aaguinagalde@transmandu.com>
2025-12-11 11:21:34 -0400

    feat: rename bulletin to safetyBulletin

--- hooks/sms/boletin/useGetBulletins.ts
+++ hooks/sms/boletin/useGetBulletins.ts
@@ -2,15 +2,15 @@ import axiosInstance from "@/lib/axios";
 import { SafetyBulletin } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 
-const fetchBulletins = async (company?: string): Promise<SafetyBulletin[]> => {
+const fetchSafetyBulletins = async (company?: string): Promise<SafetyBulletin[]> => {
   const { data } = await axiosInstance.get(`/${company}/sms/bulletin`);
   return data;
 };
 
-export const useGetBulletins = (company?: string) => {
+export const useGetSafetyBulletins = (company?: string) => {
   return useQuery<SafetyBulletin[]>({
     queryKey: ["safety-bulletins", company],
-    queryFn: () => fetchBulletins(company),
+    queryFn: () => fetchSafetyBulletins(company),
     staleTime: 1000 * 60 * 5, // 5 minutos
     enabled: !!company,
   });
