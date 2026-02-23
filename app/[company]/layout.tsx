3b1d663284e5ae9c7a3cf14593f75e6d09a22a15
Tairon Rojas <trojas@transmandu.com>
2026-02-23 16:50:31 -0400

    fix: Don't show Sidebar if SMS page is opened

--- app/[company]/layout.tsx
+++ app/[company]/layout.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import DashboardLayout from '@/components/layout/DashboardLayout';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
-const RoutesLayout = ({children}: {
-    children: React.ReactNode
+const RoutesLayout = ({ children }: {
+  children: React.ReactNode
 }) => {
+  const pathname = usePathname();
+  const isPublicPage = pathname?.includes('/general/reporte/pagina_de_sms');
+
+  if (isPublicPage) {
+    return <>{children}</>;
+  }
+
   return (
     <DashboardLayout>{children}</DashboardLayout>
   )
