19d3c9cafa03ec84cc573e9bd153487c71267249
Benjamin Ortiz <bortiz@transmandu.com>
2026-03-13 09:15:51 -0400

    feat: changes desing menu

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -23,7 +23,7 @@ export function Sidebar() {
     >
       <SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} />
       <div className="relative h-full flex flex-col px-3 py-4 overflow-y-auto shadow-md dark:shadow-zinc-800 mt-5">
-        <div className="bg-white rounded-md px-2 pt-2 pb-4 mb-1 mt-4 shadow-sm flex justify-center">
+        <div className="bg-background rounded-md px-2 pt-2 pb-4 mb-1 mt-4 shadow-sm dark:shadow-zinc-900 flex justify-center">
           <Button
             className={cn(
               "transition-transform ease-in-out duration-300 w-full justify-center",
