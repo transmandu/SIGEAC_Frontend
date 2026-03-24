b153de361b3ae3834bafa0d7893329d313556a7a
aeaf-transmandu <aaguinagalde@transmandu.com>
2026-03-24 11:07:26 -0400

    feat: config main page for sms omac

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http", // O "https" si tiene certificado
-        hostname: "172.190.0.162",
-      },
-      {
-        protocol: "https",
-        hostname: "apisigeactmd74.share.zrok.io",
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "http", // O "https" si tiene certificado
+                hostname: "172.190.0.162",
+            },
+            {
+                protocol: "http", // O "https" si tiene certificado
+                hostname: "172.21.161.72",
+            },
+
+            {
+                protocol: "https",
+                hostname: "apisigeactmd74.share.zrok.io",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
