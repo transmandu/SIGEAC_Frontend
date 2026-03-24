009a059a645976e5d404e0943f7e73e5ebbadea9
Angel Anton <angeljanton@gmail.com>
2026-03-24 11:04:15 -0400

    fix: typos error

--- hooks/mantenimiento/compras/useGetRequisitionByOrderNumber.ts
+++ hooks/mantenimiento/compras/useGetRequisitionByOrderNumber.ts
@@ -1,45 +1,62 @@
 import axios from '@/lib/axios';
-import { AdministrationRequisition, Convertion, Unit, User } from '@/types';
+import { Aircraft, Unit, User } from '@/types';
 import { useQuery } from '@tanstack/react-query';
 
-interface Requisition {
-  id: number,
-  status: string,
-  created_by: User,
-  requested_by: string,
-  received_by: string,
-  image?: string,
-  justification: string,
-  arrival_date: Date,
-  submitted_date: Date,
+export interface RequisitionByOrderNumber {
+  id: number;
+  order_number: string;
+  status: string;
+  type: 'GENERAL' | 'AERONAUTICO';
+  created_by: User;
+  requested_by: string;
+  received_by: string;
+  image?: string;
+  justification: string;
+  arrival_date?: Date;
+  submission_date?: Date;
+  submitted_date?: Date;
+  aircraft?: Aircraft;
   batch: {
-    id: number,
-    name: string,
+    id: number;
+    name: string;
     batch_articles: {
-      article_part_number: string,
-      article_alt_part_number?: string,
-      pma?: string,
-      manual?: string,
-      reference_cod?: string,
-      justification: string,
-      quantity: number,
-      unit?: Unit,
-      image?: string,
-      certificates?: string[]
-    }[]
-  }[]
-}[]
+      article_part_number: string;
+      article_alt_part_number?: string;
+      pma?: string;
+      manual?: string;
+      reference_cod?: string;
+      justification?: string;
+      quantity: number;
+      unit?: Unit;
+      image?: string;
+      certificates?: string[];
+    }[];
+  }[];
+}
 
-
-const fetchRequisitionByOrderNumber = async ({company, order_number}:{company: string | undefined, order_number: string}): Promise<Requisition> => {
-  const {data} = await axios.get(`/${company}/show-requisition-order/${order_number}`);
+const fetchRequisitionByOrderNumber = async ({
+  company,
+  order_number,
+}: {
+  company: string | undefined;
+  order_number: string;
+}): Promise<RequisitionByOrderNumber> => {
+  const { data } = await axios.get(
+    `/${company}/show-requisition-order/${order_number}`
+  );
   return data[0];
 };
 
-export const useGetRequisitionByOrderNumber = ({company, order_number}:{company: string | undefined, order_number: string}) => {
-  return useQuery<Requisition, Error>({
-    queryKey: ["requisition-order", company, order_number],
-    queryFn: () => fetchRequisitionByOrderNumber({company, order_number}),
+export const useGetRequisitionByOrderNumber = ({
+  company,
+  order_number,
+}: {
+  company: string | undefined;
+  order_number: string;
+}) => {
+  return useQuery<RequisitionByOrderNumber, Error>({
+    queryKey: ['requisition-order', company, order_number],
+    queryFn: () => fetchRequisitionByOrderNumber({ company, order_number }),
     enabled: !!company && !!order_number,
   });
 };
