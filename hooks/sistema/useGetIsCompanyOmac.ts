614b824d4ca10248f025de2c985a3da98ce2ae57
aeaf-transmandu <aaguinagalde@transmandu.com>
2026-03-25 14:39:18 -0400

    feat: to know is the company is omac

---
+++ hooks/sistema/useGetIsCompanyOmac.ts
@@ -0,0 +1,16 @@
+import axiosInstance from "@/lib/axios";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchIsCompanyOmac = async (company: string) => {
+    const { data } = await axiosInstance.get(`/${company}/is-omac`);
+    return data.data;
+};
+
+export const useGetIsCompanyOmac = (company: string) => {
+    return useQuery<boolean>({ // Especificamos que el resultado es boolean
+        queryKey: ["company-is-omac", company],
+        queryFn: () => fetchIsCompanyOmac(company),
+        staleTime: 1000 * 60 * 5,
+        enabled: !!company,
+    });
+};
