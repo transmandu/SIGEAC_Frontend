d4be3d35e1b9187061af4aff27f816555983515e
Angel Anton <angeljanton@gmail.com>
2026-03-31 14:20:14 -0400

    angel

--- actions/mantenimiento/planificacion/vuelos/actions.ts
+++ actions/mantenimiento/planificacion/vuelos/actions.ts
@@ -17,22 +17,22 @@ export const useCreateFlightControl = () => {
   const queryClient = useQueryClient()
 
   const createMutation = useMutation({
-      mutationFn: async ({data, company}: {data: CreateFlightControlData, company: string}) => {
-          await axiosInstance.post(`/${company}/flight-control`, data)
-        },
-      onSuccess: () => {
-          queryClient.invalidateQueries({queryKey: ['flight-control']})
-          toast.success("¡Creado!", {
-              description: `El vuelo ha sido registrado correctamente.`
-          })
-        },
-      onError: (error) => {
-          toast.error('Oops!', {
-            description: 'No se pudo registrar el vuelo...'
-          })
-          console.log(error)
-        },
-      }
+    mutationFn: async ({ data, company }: { data: CreateFlightControlData, company: string }) => {
+      await axiosInstance.post(`/${company}/flight-control`, data)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['flight-control'] })
+      toast.success("¡Creado!", {
+        description: `El vuelo ha sido registrado correctamente.`
+      })
+    },
+    onError: (error) => {
+      toast.error('Oops!', {
+        description: 'No se pudo registrar el vuelo...'
+      })
+      console.log(error)
+    },
+  }
   )
   return {
     createFlightControl: createMutation,
@@ -44,22 +44,22 @@ export const useUpdateFlightControl = () => {
   const queryClient = useQueryClient()
 
   const updateMutation = useMutation({
-      mutationFn: async ({id, data, company}: {id: string, data: Partial<CreateFlightControlData>, company: string}) => {
-          await axiosInstance.put(`/${company}/flight-control/${id}`, data)
-        },
-      onSuccess: () => {
-          queryClient.invalidateQueries({queryKey: ['flight-control']})
-          toast.success("¡Actualizado!", {
-              description: `El vuelo ha sido actualizado correctamente.`
-          })
-        },
-      onError: (error) => {
-          toast.error('Oops!', {
-            description: 'No se pudo actualizar el vuelo...'
-          })
-          console.log(error)
-        },
-      }
+    mutationFn: async ({ id, data, company }: { id: string, data: Partial<CreateFlightControlData>, company: string }) => {
+      await axiosInstance.put(`/${company}/flight-control/${id}`, data)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['flight-control'] })
+      toast.success("¡Actualizado!", {
+        description: `El vuelo ha sido actualizado correctamente.`
+      })
+    },
+    onError: (error) => {
+      toast.error('Oops!', {
+        description: 'No se pudo actualizar el vuelo...'
+      })
+      console.log(error)
+    },
+  }
   )
   return {
     updateFlightControl: updateMutation,
