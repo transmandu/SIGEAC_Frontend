55d82c1e50472b25a5293e9265f3a0e5312d4695
Alvaro Aguinagalde <alvaro.aguinagalde@gmail.com>
2026-04-28 09:24:18 -0400

    feat: set dynamic voluntary report form

--- app/[company]/general/reporte/voluntario/page.tsx
+++ app/[company]/general/reporte/voluntario/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 
 import { CreateVoluntaryReportForm } from "@/components/forms/aerolinea/sms/CreateVoluntaryReportForm";
+import { CreateGenVolReport } from "@/components/forms/mantenimiento/sms/CreateGenVolReport";
 import { ContentLayout } from "@/components/layout/ContentLayout";
+import { useCompanyStore } from "@/stores/CompanyStore";
 
 const CreateVoluntaryReport = () => {
-  return (
-    <ContentLayout title="Creacion de Reporte Voluntario">
-      <div className="flex flex-col justify-center items-center">
-        <CreateVoluntaryReportForm
-          onClose={() => false}
-        ></CreateVoluntaryReportForm>
-      </div>
-    </ContentLayout>
-  );
+    const { selectedCompany } = useCompanyStore();
+
+    const Component = selectedCompany?.isOmac ? CreateGenVolReport : CreateVoluntaryReportForm;
+    return (
+        <ContentLayout title="Creación de Reporte Voluntario">
+            <div className="flex flex-col justify-center items-center">
+                <Component onClose={() => false} />
+            </div>
+        </ContentLayout>);
 };
 
 export default CreateVoluntaryReport;
