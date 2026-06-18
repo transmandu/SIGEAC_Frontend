23bd43f3ede7510048ee67e9306581f0b00bea12
alvaro <alvaro.aguinagalde@gmail.com>
2026-06-18 14:38:16 -0400

    feat: too add many flights in the same form

--- actions/mantenimiento/planificacion/vuelos/actions.ts
+++ actions/mantenimiento/planificacion/vuelos/actions.ts
@@ -17,18 +17,19 @@ export const useCreateFlightControl = () => {
   const queryClient = useQueryClient()
 
   const createMutation = useMutation({
-    mutationFn: async ({ data, company }: { data: CreateFlightControlData, company: string }) => {
-      await axiosInstance.post(`/${company}/flight-control`, data)
+    mutationFn: async ({ data, company }: { data: CreateFlightControlData | CreateFlightControlData[], company: string }) => {
+      const flights = Array.isArray(data) ? data : [data]
+      await axiosInstance.post(`/${company}/flight-control`, { flights })
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['flight-control'] })
       toast.success("¡Creado!", {
-        description: `El vuelo ha sido registrado correctamente.`
+        description: `El/los vuelo(s) ha(n) sido registrado(s) correctamente.`
       })
     },
     onError: (error) => {
       toast.error('Oops!', {
-        description: 'No se pudo registrar el vuelo...'
+        description: 'No se pudo registrar el/los vuelo(s)...'
       })
       console.log(error)
     },
