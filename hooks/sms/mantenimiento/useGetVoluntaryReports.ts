380e3f498986c9a1c68846d905de2a5cb99d24fc
alvaro <alvaro.aguinagalde@gmail.com>
2026-07-15 09:59:50 -0400

    feat: invalidate query with company

--- hooks/sms/mantenimiento/useGetVoluntaryReports.ts
+++ hooks/sms/mantenimiento/useGetVoluntaryReports.ts
@@ -3,17 +3,17 @@ import { VoluntaryReport } from "@/types/sms/mantenimiento";
 import { useQuery } from "@tanstack/react-query";
 
 const fetchVoluntaryReports = async (
-    company?: string
+  company?: string
 ): Promise<VoluntaryReport[]> => {
-    const { data } = await axiosInstance.get(`/${company}/sms/aeronautical/voluntary-reports`);
-    return data;
+  const { data } = await axiosInstance.get(`/${company}/sms/aeronautical/voluntary-reports`);
+  return data;
 };
 
 export const useGetVoluntaryReports = (company?: string) => {
-    return useQuery<VoluntaryReport[]>({
-        queryKey: ["voluntary-reports"],
-        queryFn: () => fetchVoluntaryReports(company),
-        staleTime: 1000 * 60 * 5, // 5 minutos
-        enabled: !!company,
-    });
+  return useQuery<VoluntaryReport[]>({
+    queryKey: ["voluntary-reports", company],
+    queryFn: () => fetchVoluntaryReports(company),
+    staleTime: 1000 * 60 * 5, // 5 minutos
+    enabled: !!company,
+  });
 };
